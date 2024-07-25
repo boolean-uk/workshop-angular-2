eec45b6f6afafb5486932496fe0c356abe9aea17
Lukas <lad_777@hotmail.com>
2024-07-25 15:22:23 +0100

    fix: added imports, routing module and added providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { AppComponent } from './app.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { DataComponent } from './components/data/data.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { AppShellComponent } from './shared/layout/app-shell/app-shell.component';
+import { AppRoutingModule } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
     declarations: [
@@ -23,8 +19,10 @@ import { AppShellComponent } from './shared/layout/app-shell/app-shell.component
         HttpClientModule,
         BrowserModule,
         RouterOutlet,
-        SharedModule
+        SharedModule,
+        AppRoutingModule
         ],
+    providers: [provideAnimationsAsync()],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
