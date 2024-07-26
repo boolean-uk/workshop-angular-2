ae823fa2c72d8e991b3cdce62064ba6b69908f94
Lukas <lad_777@hotmail.com>
2024-07-26 12:49:00 +0100

    feat: added constructor to call data from AppComponent in Users index

--- src/app/features/user/pages/index/index.component.ts
+++ src/app/features/user/pages/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../../../../app.component';
 
 @Component({
   selector: 'app-index',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class IndexComponent {
 
+  constructor (public appData: AppComponent){}
 }
